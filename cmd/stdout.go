8447d2dbbd885bce01f49294c4c9ab52bf773f0f
Will Fitzgerald <will.fitzgerald@pobox.com>
2016-03-23 11:25:10 -0400

    initial commit

---
+++ cmd/stdout.go
@@ -0,0 +1,52 @@
+// Copyright © 2016 Will Fitzgerald <will@archive.org>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// stdoutCmd represents the stdout command
+var stdoutCmd = &cobra.Command{
+	Use:   "stdout",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("stdout called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(stdoutCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// stdoutCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// stdoutCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+}
